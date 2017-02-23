bed39a46d92376ddeb7e8c62b5c8d78f07e4b077
Italo Di Renzo <italodirenzo93@gmail.com>
2017-02-22 19:45:54 -0500

    Moving enemy movement into pattern scripts

---
+++ Assets/Scripts/SineWavePattern.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SineWavePattern : MonoBehaviour
+{
+    public float verticalVelocity;
+    public float horizontalVelocity;
+
+    [SerializeField]
+    public float maximumWavelength;
+
+    private Rigidbody2D m_rigidBody;
+
+    private void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody2D>();
+    }
+    
+	private void FixedUpdate()
+    {
+        float xPos = m_rigidBody.position.x - (horizontalVelocity * Time.fixedDeltaTime);
+        float yPos = Mathf.PingPong(verticalVelocity * Time.fixedTime, maximumWavelength) - (maximumWavelength / 2);
+        m_rigidBody.MovePosition(new Vector2(xPos, yPos));
+    }
+}
